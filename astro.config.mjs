d907e633720ea14aecb517ccd11d13dbfebc76ce
Karl Mc Carron <karlmccarron@cantab.net>
2023-11-14 12:33:01 +0000

    Got math displaying correctly in blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,19 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+
+import remarkMath from 'remark-math'
+import rehypeMathjax from 'rehype-mathjax'
+import rehypeKatex from 'rehype-katex'
+
 import { remarkReadingTime } from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
+	site: 'https://karl-mccarron.github.io/', // Write here your website url
 	markdown: {
-		remarkPlugins: [remarkReadingTime],
+		remarkPlugins: ['remark-gfm', 'remark-smartypants', remarkMath, remarkReadingTime],
+		rehypePlugins: [rehypeKatex,],
 		drafts: true,
 		shikiConfig: {
 			theme: 'material-theme-palenight',
---
+++ src/content/blog/astro copy 2.md
@@ -0,0 +1,27 @@
+---
+title: 'MacBook Pro 2022'
+description: >-
+  The new MacBook Pro 2022 is here. With the Apple M2 chip, a new design, and
+  more, the new MacBook Pro is the best laptop Apple has ever made.
+pubDate: 2022-07-01T22:00:00.000Z
+heroImage: ../../assets/images/bg.jpg
+category: Category 1
+tags:
+  - JavaScript
+  - css
+  - HTML5
+  - GitHub
+---
+
+# Title
+
+Lift($L$) can be determined by Lift Coefficient ($C_L$) like the following
+equation.
+
+$
+L = \frac{1}{2} \rho v^2 S C_L
+$
+
+$
+1 + 2 \ne 3
+$
